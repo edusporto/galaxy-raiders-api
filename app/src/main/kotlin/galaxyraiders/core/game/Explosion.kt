be5062f3547c4d4116bdff8d6968833da0b71889
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-07-28 01:10:15 -0300

    Added Explosion abstraction

---
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -0,0 +1,13 @@
+package galaxyraiders.core.game
+
+import galaxyraiders.core.physics.Point2D
+import galaxyraiders.core.physics.Vector2D
+
+class Explosion(
+//  isTriggered: Boolean,
+  initialPosition: Point2D,
+  initialVelocity: Vector2D,
+  radius: Double,
+  mass: Double
+) :
+  SpaceObject("Explosion", 'X', initialPosition, initialVelocity, radius, mass)
